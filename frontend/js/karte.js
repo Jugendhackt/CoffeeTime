1d99159f928b881099430bdfd6470f72ccc91af5
elisabethschiele <elisabethschiele@gmail.com>
2018-05-12 18:04:04 +0200

    marker mit backend

--- frontend/js/karte.js
+++ frontend/js/karte.js
@@ -1,15 +1,14 @@
 var mymap = L.map('mapid').setView([50.941,6.958], 13);
 var markers=[];
 
-function addMarker(x,y){
-  var marker = L.marker([x, y]).addTo(mymap);
+function addMarker(location){
+  var marker = L.marker([location.long,location.lat]).addTo(mymap);
   markers.push(marker);
-  marker.on('click', showPopup)
+  marker.on('click', function (){
+    marker.bindPopup('<b>'+location.name+'</b><br>I am a popup</b><br><a href="https://www.google.de">more information</a>').openPopup();
+  })
 }
-function showPopup(e){
-  e.target.bindPopup('<b>Hello world!</b><br>I am a popup</b><br><a href="https://www.google.de">more information</a>').openPopup();
-}
-addMarker(50.941,6.958);
+
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -17,3 +16,12 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
     id: 'mapbox.streets',
     accessToken: 'pk.eyJ1IjoiZWxpc2FiZXRoc2NoaWVsZSIsImEiOiJjamdpMnFpcXowaWJwMnFwams4aWk5YXl3In0.IuvGOgBuRE6_BMIkhE7Okg'
 }).addTo(mymap);
+
+fetch('http://10.23.42.102:8081/getAll')
+  .then(function(response){
+    return response.json();
+  })
+  .then(function(myJson){
+    console.log(myJson);
+    myJson.forEach(addMarker)
+  });
